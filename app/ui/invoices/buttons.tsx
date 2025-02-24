cf486490b6fb28bba53a930ccd731bf812ea3da1
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-02-24 09:37:12 -0400

    Invoices - Delete implemented.

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,7 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/actions';
+import { useCallback } from 'react';
 
 export function CreateInvoice() {
   return (
@@ -25,12 +27,13 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = useCallback(deleteInvoice.bind(null, id), [id]);
   return (
-    <>
+    <form action={deleteInvoiceWithId}>
       <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
-    </>
+    </form>
   );
 }
