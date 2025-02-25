e13f233377a7df32760dd8ad85ba73e8b9d5fdb2
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2025-02-24 21:08:33 -0400

    Create Invoice Form - Missing aria properties added.

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -70,6 +70,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
                 // required
               />
               <CurrencyDollarIcon
@@ -99,6 +100,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="pending"
@@ -114,6 +116,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="paid"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="paid"
